ba031f1e79c393d596e38ea9dae16f16fc6aed97
Gopistol <rhrjs0131@naver.com>
2023-11-13 23:26:00 +0900

    feat: 메뉴 종류별로 enum 적용

---
+++ src/main/java/christmas/domain/menu/category/Appetizer.java
@@ -0,0 +1,34 @@
+package christmas.domain.menu.category;
+
+import core.ErrorMessage;
+
+public enum Appetizer {
+
+  양송이수프(6_000),
+  타파스(5_500),
+  시저샐러드(8_000);
+
+  private final int prize;
+
+  Appetizer(int prize) {
+    this.prize = prize;
+  }
+
+  public static boolean contains(String menu) {
+    for (Appetizer appetizer : Appetizer.values()) {
+      if (appetizer.name().equals(menu)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int getPrize(String menu) {
+    for (Appetizer appetizer : Appetizer.values()) {
+      if (appetizer.name().equals(menu)) {
+        return appetizer.prize;
+      }
+    }
+    throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_NAME + menu);
+  }
+}
---
+++ src/main/java/christmas/domain/menu/category/Drink.java
@@ -0,0 +1,34 @@
+package christmas.domain.menu.category;
+
+import core.ErrorMessage;
+
+public enum Drink {
+
+  제로콜라(3_000),
+  레드와인(60_000),
+  샴페인(25_000);
+
+  private final int prize;
+
+  Drink(int prize) {
+    this.prize = prize;
+  }
+
+  public static boolean contains(String menu) {
+    for (Drink drink : Drink.values()) {
+      if (drink.name().equals(menu)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static int getPrize(String menu) {
+    for (Drink drink : Drink.values()) {
+      if (drink.name().equals(menu)) {
+        return drink.prize;
+      }
+    }
+    throw new IllegalArgumentException(ErrorMessage.INVALID_MENU_NAME + menu);
+  }
+}
