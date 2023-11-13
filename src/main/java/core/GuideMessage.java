ae9c27c6ac9ab08da62f80cda9c18adddc9b2bd2
Gopistol <rhrjs0131@naver.com>
2023-11-13 23:28:30 +0900

    feat: 안내 메시지, 오류 메시지, 기타 상수 추가

---
+++ src/main/java/core/GuideMessage.java
@@ -0,0 +1,16 @@
+package core;
+
+public class GuideMessage {
+
+  public final static String INTRO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n";
+  public final static String EXPECTED_VISIT_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
+  public final static String ORDER_MENU_NAME_AMOUNT = "주문하실 메뉴의 이름과 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n";
+  public final static String PREVIEW_EVENT_BENEFITS = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
+  public final static String ORDER_MENU = "<주문 메뉴>\n";
+  public final static String TOTAL_PRICE_BEFORE_DISCOUNT = "<할인 전 총주문 금액>\n";
+  public final static String GIVEAWAY_MENU = "<증정 메뉴>\n";
+  public final static String BENEFIT_DETAILS = "<혜택 내역>\n";
+  public final static String TOTAL_BENEFIT_AMOUNT = "<총혜택 금액>\n";
+  public final static String EXPECTED_PAYMENT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>\n";
+  public final static String EVENT_BADGE = "<12월 이벤트 배지>\n";
+}
